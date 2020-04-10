e72545f89de49d9c1a9bfb7d0d5daf946ea19631
EileenJuergens <juergens.eileen@gmail.com>
2020-04-10 14:34:07 +0200

    feat: finish part 2.5 - course contents

--- Part-2/course-contents/src/App.js
+++ Part-2/course-contents/src/App.js
@@ -1,45 +1,5 @@
 import React from 'react';
-
-const Total = ({ parts }) => {
-
-const reducer = (acc, curr) => acc + curr;
-const totalValue = parts.map(el => el.exercises).reduce(reducer)
-
-return <b>Number of exercises {totalValue}</b>;
-}
-
-const Part = ({ name, number }) => {
-
-  return <p>{name} {number}</p>;
-}
-
-
-const Content = ({ parts }) => {
-
-  const content = () => parts.map(part => {
-    return <Part key={part.id} name={part.name} number={part.exercises} />;
-  })
-
-  return content();
-}
-
-
-const Header = ({ name }) => {
-
-  return <h2>{name}</h2>;
-}
-
-
-const Course = ({ course }) => {
-
-  return (
-    <>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  )
-}
+import Course from './Course';
 
 
 const App = () => {
