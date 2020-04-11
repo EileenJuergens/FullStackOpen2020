e583cc3f0ec5183279816ac88bcd41d11a5183ba
EileenJuergens <juergens.eileen@gmail.com>
2020-04-11 12:52:41 +0200

    feat: finish part 2.8 - phonebook

--- Part-2/phonebook/src/App.js
+++ Part-2/phonebook/src/App.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '689-603-229' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addPerson = event => {
     event.preventDefault()
     const personObject = {
       name: newName,
+      number: newNumber
     }
 
     const dobbledName = persons.find(person => person.name === newName)
@@ -19,10 +21,15 @@ const App = () => {
       : setPersons(persons.concat(personObject))
 
     setNewName('')
+    setNewNumber('')
   }
   
-  const handleInputChange = event => {
+  const handleNameChange = event => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = event => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -30,7 +37,8 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleInputChange}/>
+          name: <input value={newName} onChange={handleNameChange}/><br/>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -39,7 +47,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((person, i) => 
-         <li key={i}>{person.name}</li>
+         <li key={i}>{person.name} {person.number}</li>
         )}
       </ul>
     </div>
