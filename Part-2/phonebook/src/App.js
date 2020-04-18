a559b0b785f5a76f57c8ae0a324203a844749777
EileenJuergens <juergens.eileen@gmail.com>
2020-04-18 17:33:59 +0200

    feat: finish part 2.18 - phonebook

--- Part-2/phonebook/src/App.js
+++ Part-2/phonebook/src/App.js
@@ -30,30 +30,63 @@ const App = () => {
 
   const addPerson = event => {
     event.preventDefault()
+
     const personObject = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1
+      id: newName
     }
-    const dobbledName = persons.find(person => person.name === newName)
 
-    dobbledName
-      ? alert(`${newName} is already added to phonebook`)
-      : setPersons(persons.concat(personObject))
+    const dobbledPerson = persons.find(person => (
+      person.name.toLowerCase() === newName.toLowerCase() && person.number === newNumber
+    ))
 
-    personService
-      .createOne(personObject)
-      .then(returnedPerson => {
-        setPersons(persons.concat(returnedPerson))
-        setNewName('')
-        setNewNumber('')
-      })
+    const dobbledPersonNumber = persons.find(person => (
+      person.name.toLowerCase() === newName.toLowerCase() && person.number !== newNumber
+    ))
+
+    // case if person exsist already 
+    if (dobbledPerson) {
+      alert(`${dobbledPerson.name} is already added to phonebook`)
+    }
+
+    // case if number from person should be updated
+    else if (dobbledPersonNumber) {
+      const message = `${dobbledPersonNumber.name} is already in the phonebook, do you want to replace the old number with the new one?`
+      const confirmed = window.confirm(message);
+
+      if (confirmed) {
+        personService
+          .updateOne(dobbledPersonNumber.id, {
+            name: dobbledPersonNumber.name,
+            number: newNumber
+          })
+          .then((data) => {
+            setPersons(persons.map(person => person.id === data.id ? data : person))
+            setNewName('');
+            setNewNumber('');
+          })
+      }
+    }
+
+    // normal create new person case
+    else {
+      personService
+        .createOne(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+        })
+    }
   }
 
 
+
   const deletePerson = (id, name) => {
-    const confirmed = window.confirm(`Do you want to delete ${name}?`)
-    if(confirmed) {
+    const message = `Do you want to delete ${name}?`
+    const confirmed = window.confirm(message)
+    if (confirmed) {
       personService
         .deleteOne(id)
         .then(() => {
--- Part-2/phonebook/src/services/persons.js
+++ Part-2/phonebook/src/services/persons.js
@@ -16,4 +16,9 @@ const deleteOne = (id) => {
   return request.then(response => response.data)
 }
 
-export default { getAll, createOne, deleteOne }
+const updateOne = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+export default { getAll, createOne, deleteOne, updateOne }
